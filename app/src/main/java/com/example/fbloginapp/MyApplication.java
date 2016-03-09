5034134c6e07ff7f4c6b334b9f4e8458ceab9b97
IMSSAM <codingssam@gmail.com>
2016-03-09 13:14:23 +0900

    Initial commit

---
+++ app/src/main/java/com/example/fbloginapp/MyApplication.java
@@ -0,0 +1,46 @@
+package com.example.fbloginapp;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import com.facebook.FacebookSdk;
+import com.facebook.LoggingBehavior;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (BuildConfig.DEBUG) {
+            FacebookSdk.setIsDebugEnabled(true);
+            FacebookSdk.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
+        }
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        printKeyHash();
+    }
+
+    /**
+     * Call this method inside onCreate once to get your hash key
+     */
+    public void printKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.example.fbloginapp", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("FBLogin", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+}
